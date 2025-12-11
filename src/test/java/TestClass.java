f2cbe6d3efff94de622e3f33b1686050aac03ca1
Uzair Khan <khanuzair54670@gmail.com>
2025-12-11 21:26:55 +0530

    first commit

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+	@Test
+	public void firstMethod() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+	}
+}
